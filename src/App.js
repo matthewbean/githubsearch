cecd2eb6a8240cad735c98e39b94bcf237fb2eed
Matt <matthew.bean@jessup.edu>
2020-01-01 11:26:41 -0800

    finished project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/home';
+import Alert from './components/layout/alert';
+import About from './components/pages/about';
+import User from './components/users/user';
+import NotFound from './components/pages/NotFound'
 
-function App() {
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+
+
+import './App.css';
+const App = () => {
+
+
+
+//get users repos
+
+
+
   return (
+    <GithubState>
+      <AlertState>
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className = 'container'>
+        <Alert />
+        <Switch>
+          <Route exact path = '/' component = {Home} />
+          <Route exact path='/about' component = {About} />
+          <Route exact path='/user/:login' component = {User} />
+          <Route component = {NotFound} />
+        </Switch>
+
+      </div>
     </div>
+    </Router>
+    </AlertState>
+    </GithubState>
   );
 }
+
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+ const Navbar = ({ icon, title })=> {
+        return (
+            <nav className = "navbar bg-primary">
+                <h1><i className = {icon} />
+            {title}</h1>
+            <ul>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+            </ul>
+            </nav>
+        )
+}
+
+Navbar.defaultProps = {
+    title: 'Githib Finder',
+    icon: 'fab fa-github'
+}
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
+
+export default Navbar
